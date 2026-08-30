07417742cb618f0941e3422615d8ac3892cfd6ba
Abdul Ghani Butt <agbutt@widener.edu>
2026-08-30 11:35:23 -0400

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.py
@@ -0,0 +1,21 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = {}
+        groupedAnagrams = []
+
+        for str in strs:
+            ana = ''.join(sorted(str))
+
+            if ana in anagrams:
+                anagrams[ana].append(str)
+            else:
+                anagrams[ana] = []
+                anagrams[ana].append(str)
+        
+        for groupings in anagrams.values():
+            groupedAnagrams.append(groupings)
+        
+        return groupedAnagrams
+
+        
+        
